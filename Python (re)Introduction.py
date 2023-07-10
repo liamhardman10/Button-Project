6b6fe2ff169073768de41eacabbee977c4c89388
Liam Hardman <liamhardman300@gmail.com>
2023-07-10 02:58:24 -0700

    Add files via upload

---
+++ Python (re)Introduction.py
@@ -0,0 +1,61 @@
+from tkinter import *
+import webbrowser
+
+# in this project im going to make buttons that link websites to them.
+
+parentwindow = Tk()
+parentwindow.geometry('990x540')
+parentwindow.title("webpage")
+
+def open():
+    webbrowser.open("https://github.com/liamhardman10")
+
+
+def click():
+    print("button has been clicked.")
+
+
+photo1 = PhotoImage(file=r"C:\Users\Liam\Downloads\GitHub.png")
+
+button1 = Button(parentwindow,
+                text="My GitHub ",
+                command=open,
+                font=("Helvetica", 50),
+                fg="white",
+                bg="#554e74",
+                activeforeground="#b0b0b0",
+                activebackground="#38334c",
+                state=ACTIVE,
+                image=photo1,
+                compound='right')
+                
+photo2 = PhotoImage(file=r"C:\Users\Liam\Downloads\LinkedIn.png")
+
+
+button2 = Button(parentwindow,
+                text="My LinkedIn ",
+                command=open,
+                font=("Helvetica", 50),
+                fg="white",
+                bg="#554e74",
+                activeforeground="#b0b0b0",
+                activebackground="#38334c",
+                state=ACTIVE,
+                image=photo2,
+                compound='right')
+
+if button2 == click(
+    open(),
+    webbrowser.open("https://www.linkedin.com/in/liam-hardman-451a7a262/")
+):
+
+
+
+    
+
+
+button1.pack()
+
+button2.pack()
+
+parentwindow.mainloop()
